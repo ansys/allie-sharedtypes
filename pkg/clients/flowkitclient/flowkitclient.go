1da0b9c46b9e77b34c4ee9f09ea0997a342d2db5
FelixKuhnAnsys <felix.kuhn@ansys.com>
2024-11-20 16:18:48 +0100

    Update flowkitclient.go

--- pkg/clients/flowkitclient/flowkitclient.go
+++ pkg/clients/flowkitclient/flowkitclient.go
@@ -35,9 +35,6 @@ func ListFunctionsAndSaveToInteralStates(ctx *logging.ContextMap) error {
 		}
 	}()
 
-	// Initialize the available functions map
-	AvailableFunctions = make(map[string]*sharedtypes.FunctionDefinition)
-
 	// Set up a connection to the server.
 	c, conn, err := createClient()
 	if err != nil {
