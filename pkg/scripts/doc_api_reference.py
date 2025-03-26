2dc42fbe22abf2eccd5ddce90062584a583eca54
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2025-03-26 09:17:56 +0100

    fix: revert asset name

--- pkg/scripts/doc_api_reference.py
+++ pkg/scripts/doc_api_reference.py
@@ -14,7 +14,7 @@ repo_owner = sys.argv[1]
 repo_name = sys.argv[2]
 
 # Set environment variables for file paths
-DOC_BUILD_HTML = "doc/_build/html"
+DOC_BUILD_HTML = "documentation-html"
 SOURCE_FILE = os.path.join(DOC_BUILD_HTML, "api_reference", "test", "index.html")
 SOURCE_DIRECTORY = f"dist/pkg/github.com/{repo_owner}/{repo_name}/pkg/"
 REPLACEMENT_DIRECTORY = os.path.join(DOC_BUILD_HTML, "api_reference", "pkg")
